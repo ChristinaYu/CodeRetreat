76b0e853ce869492495b3402dbebf22cda0b0316
amphy <amphy.kit@gmail.com>
2015-06-19 18:52:49 -0400

    unit test example

--- PrintTest.java
+++ PrintTest.java
@@ -3,8 +3,14 @@ import org.junit.Test;
 public class PrintTest
 {
 	@Test
-	public void multiplicationOfZeroIntegersShouldReturnZero() {
+	public void test_checkAlive {
 
-    //test goes here
+	int[][] board = new int[5][5];
+	board = board.allAlive();
+	for(int i = 0; i < size; i++){
+		for(int j = 0; j < size; j++){
+			assertEquals(1, board[i][j]);
+		}
+	}
   }
 }
